4629917021780d3bd27f31afcb113a3faf9b70cf
hgoui <hgouiran@gmail.com>
2023-01-30 15:10:45 +0100

    mergi

---
+++ app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/AddPostActivity.kt
@@ -0,0 +1,14 @@
+package fr.isen.zouavesteam.isensocialnetwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class AddPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post)
+
+
+    }
+}
