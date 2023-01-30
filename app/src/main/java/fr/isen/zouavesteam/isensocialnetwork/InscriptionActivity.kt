7d5732321d15c5224631d4629938e82fa72c217c
Aimasu59 <corentintyteca@gmail.com>
2023-01-30 16:14:08 +0100

    Ajout connexion et inscription Page

--- app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/InscriptionActivity.kt
+++ app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/InscriptionActivity.kt
@@ -1,5 +1,7 @@
 package fr.isen.zouavesteam.isensocialnetwork
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,8 @@ class InscriptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inscription)
+        val actionBar = supportActionBar
+        actionBar?.title = "Legends of Maximus Inscription"
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#425B8A")))
     }
 }
--- app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/MainActivity.kt
+++ app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/MainActivity.kt
@@ -1,5 +1,8 @@
 package fr.isen.zouavesteam.isensocialnetwork
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -12,12 +15,25 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import fr.isen.zouavesteam.isensocialnetwork.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val actionBar = supportActionBar
+        actionBar?.title = "Legends of Maximus"
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#425B8A")))
+
+        binding.redirectInscription.setOnClickListener{
+            val intent = Intent(this, InscriptionActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
