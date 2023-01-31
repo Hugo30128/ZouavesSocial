757edc5a1ca33a9d5f55dfbb2282ffc83c727087
Aimasu59 <corentintyteca@gmail.com>
2023-01-31 10:46:33 +0100

    systéme d'inscription et de connexion relié à firebase

--- app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/InscriptionActivity.kt
+++ app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/InscriptionActivity.kt
@@ -19,7 +19,6 @@ class InscriptionActivity : AppCompatActivity() {
     // [START declare_auth]
     private lateinit var auth: FirebaseAuth
     // [END declare_auth]
-    class UserData constructor(var username: String, val pwd: String) {}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,18 +41,13 @@ class InscriptionActivity : AppCompatActivity() {
 
 
         binding.inscriptionButton.setOnClickListener(){
-            val intent= Intent(this,AddPostActivity::class.java)
             println("Clique Bouton")
             val username=binding.userNameText.getText().toString()
             var pwd=binding.userPasswordText.text.toString()
             val pwd2=binding.userPasswordText2.text.toString()
-            val User:UserData=UserData(username,pwd)
             if(username.isNotEmpty()){
                 if(pwd==pwd2){
                     createAccount(username,pwd)
-                    intent.putExtra("USER",User.username)
-                    println("Start New Activity")
-                    startActivity(intent)
                 }
             }
         }
@@ -78,50 +72,26 @@ class InscriptionActivity : AppCompatActivity() {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
                     val user = auth.currentUser
-                    updateUI(user)
+                    updateUI(user,email)
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
-                    updateUI(null)
+                    updateUI(null,email)
                 }
             }
         // [END create_user_with_email]
     }
 
-    private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    updateUI(null)
-                }
-            }
-        // [END sign_in_with_email]
-    }
+    private fun updateUI(user: FirebaseUser?, username: String) {
+        if(user!=null){
+            val intent= Intent(this,AddPostActivity::class.java)
+            intent.putExtra("USER",username)
+            startActivity(intent)
 
-    /*private fun sendEmailVerification() {
-        // [START send_email_verification]
-        val user = auth.currentUser!!
-        user.sendEmailVerification()
-            .addOnCompleteListener(this) { task ->
-                // Email Verification sent
-            }
-        // [END send_email_verification]
-    }*/
 
-    private fun updateUI(user: FirebaseUser?) {
-
+        }
     }
 
     private fun reload() {
@@ -129,6 +99,6 @@ class InscriptionActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val TAG = "EmailPassword"
+        const val TAG = "EmailPassword"
     }
 }
--- app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/MainActivity.kt
+++ app/src/main/java/fr/isen/zouavesteam/isensocialnetwork/MainActivity.kt
@@ -7,7 +7,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -20,18 +24,82 @@ import fr.isen.zouavesteam.isensocialnetwork.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // [END initialize_auth]
         val actionBar = supportActionBar
         actionBar?.title = "Legends of Maximus"
         supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#425B8A")))
+        onStart()
         binding.redirectInscription.setOnClickListener {
             val intent = Intent(this, InscriptionActivity::class.java)
             startActivity(intent)
         }
+
+        binding.connexionButton.setOnClickListener{
+            val username=binding.userNameText.text.toString()
+            val pwd=binding.userPasswordText.text.toString()
+            if(username.isNotEmpty()){
+                if(pwd.isNotEmpty()){
+                    signIn(username,pwd)
+                }
+            }
+        }
+
+
+
+    }
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+    // [END on_start_check_user]
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(InscriptionActivity.TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user,email)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(InscriptionActivity.TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null,email)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+    private fun updateUI(user: FirebaseUser?, username: String) {
+        if(user!=null){
+            val intent= Intent(this,AddPostActivity::class.java)
+            intent.putExtra("USER",username)
+            startActivity(intent)
+
+
+        }
+    }
+    private fun reload() {
+
+    }
+    companion object {
+        const val TAG = "EmailPassword"
     }
 }
